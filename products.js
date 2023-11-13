c7aebc6595cc1fa87a61ea3602256cc1dff686f0
anthonyins <132161891+anthonyins@users.noreply.github.com>
2023-11-13 23:21:52 +0300

    Update products.js

--- products.js
+++ products.js
@@ -1,6 +1,6 @@
-const textTruncate = (text, char) => {
-  return text.length > char ? text.substring(0, char) + "..." : text;
-};
+// const textTruncate = (text, char) => {
+//   return text.length > char ? text.substring(0, char) + "..." : text;
+// };
 export const productUI = (product) => {
   const { id,image, title, description } = product;
   const productDiv = document.createElement("div");
@@ -10,8 +10,8 @@ export const productUI = (product) => {
           <div class="card">
             <img src="${image}" class="p-2" height="250px" alt="...">
             <div class="card-body">
-              <h5 class="card-title">${textTruncate(title, 20)}</h5>
-              <p class="card-text">${textTruncate(description, 85)}</p>
+      <h5 class="card-title line-clamp-1">${title}</h5>
+              <p class="card-text line-clamp-3">${description}</p>
             </div>
             <div class="card-footer w-100 d-flex justify-content-center gap-3">
                 <button class="btn btn-danger">
