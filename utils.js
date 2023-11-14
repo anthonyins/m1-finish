bd48734072f1431e107b7a7daba4aa2a72e1cc34
anthonyins <132161891+anthonyins@users.noreply.github.com>
2023-11-14 12:39:20 +0300

    Update utils.js

--- utils.js
+++ utils.js
@@ -1,3 +1,14 @@
 export function addStorage(myProducts) {
     localStorage.setItem("products", JSON.stringify(myProducts));
 }
+
+export const btnColors = [
+    "primary",
+    "secondary",
+    "success",
+    "info",
+    "warning",
+    "danger",
+    "light",
+    "dark",
+  ];
